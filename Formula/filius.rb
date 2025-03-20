de729d33495e9f54f00d36cad5e85624847767b2
phoenixthrush <mr.cetto@phoenixthrush.com>
2025-03-20 20:03:03 +0100

    add README

--- Formula/filius.rb
+++ Formula/filius.rb
@@ -1,21 +1,22 @@
 class Filius < Formula
-  desc "Network simulator for educational purposes"
+  desc "Filius is a network simulator for educational purpose"
   homepage "https://www.lernsoftware-filius.de"
   head "https://gitlab.com/filius1/filius.git", branch: "master"
+  license any_of: ["GPL-2.0-or-later", "GPL-3.0-or-later"]
 
   depends_on "java"
   depends_on "maven"
 
   def install
-    system "mvn", "clean", "package"
-    libexec.install "target/filius.sh"
-    bin.write_exec_script libexec/"filius"
-  end
+    system "mvn", "clean", "package", "-DskipTests"
+    
+    libexec.install Dir["target"]
+    # bin.write_jar_script libexec/"target/filius.jar", "filius"
 
-  def caveats
-    <<~EOS
-      Filius has been installed. To run it, use:
-        filius
+    # TODO: this currently suppresses stdout completely, including the help section...
+    (bin/"filius").write <<~EOS
+      #!/bin/bash
+      exec java -jar #{libexec}/target/filius.jar "$@" > /dev/null
     EOS
   end
 end
---
+++ README.md
@@ -0,0 +1,22 @@
+# phoenixthrush homebrew-tools
+
+## How do I install these formulae?
+
+`brew install phoenixthrush/homebrew-tools/<formula>`
+
+Or `brew tap phoenixthrush/homebrew-tools` and then `brew install <formula>`.
+
+Or, in a `brew bundle` `Brewfile`:
+
+```ruby
+tap "phoenixthrush/homebrew-tools"
+brew "<formula>"
+```
+
+## Available Packages
+
+- `filius`: Filius is a network simulator for educational purpose
+
+## Documentation
+
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
