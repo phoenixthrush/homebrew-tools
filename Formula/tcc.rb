b875800a52f10fd77d10179f46f041bc4e48826d
phoenixthrush <mr.cetto@phoenixthrush.com>
2025-03-27 10:44:59 +0100

    fix tcc formula

--- Formula/tcc.rb
+++ Formula/tcc.rb
@@ -6,24 +6,51 @@ class Tcc < Formula
   head "https://repo.or.cz/tinycc.git", branch: "mob"
 
   def install
-    os_include_path = if OS.mac?
-      MacOS.sdk_path/"usr/include"
-    else
-      "/usr/include:/usr/include/x86_64-linux-gnu"
-    end
+    # os_include_path = if OS.mac?
+    #   MacOS.sdk_path/"usr/include"
+    # else
+    #   "/usr/include:/usr/include/x86_64-linux-gnu"
+    # end
 
-    args = %W[
-      --prefix=#{prefix}
-      --source-path=#{buildpath}
-      --sysincludepaths=#{HOMEBREW_PREFIX}/include:#{os_include_path}:{B}/include
-      --enable-cross
-    ]
+    # args = %W[
+    #   --prefix=#{prefix}
+    #   --source-path=#{buildpath}
+    #   --sysincludepaths=#{HOMEBREW_PREFIX}/include:#{os_include_path}:{B}/include
+    #   --enable-cross
+    # ]
 
-    ENV.deparallelize
+    # ENV.deparallelize
     system "./configure"
     inreplace "config.mak", "../../super/cc", "clang"
+    # inreplace "config.mak", "/usr/local/lib", "#{lib}"
+
     system "make"
-    system "make", "test"
-    system "make", "install"
+    # system "make", "test"
+    # system "make", "install"
+
+    bin.install Dir["tcc", "*-tcc"]
+    (lib/"tcc").install Dir["libtcc1.a", "runmain.o", "bt-exe.o", "bt-log.o", "bcheck.o", "*-libtcc1.a"]
+    (lib/"tcc/include").install Dir["include/*.h", "tcclib.h"]
+    lib.install Dir["libtcc.a", "*.so"]
+    include.install "libtcc.h"
+    man1.install Dir["tcc.1"]
+
+    if Dir.exist?("win32")
+      (lib/"tcc/win32/lib").install Dir["win32/lib/*.def", "*-win32-libtcc1.a", "*-wince-libtcc1.a"]
+      (lib/"tcc/win32/include").install Dir["win32/include/*", "include/*.h", "tcclib.h"]
+    end
+  end
+
+  def caveats
+    <<~EOS
+      To use tcc, you may need to set the following environment variables:
+
+        export LIBRARY_PATH="#{opt_lib}/tcc"
+
+      If you're using macOS, also consider adding this to your shell configuration file:
+      
+        echo 'export LIBRARY_PATH="#{opt_lib}/tcc"' >> ~/.zshrc
+        source ~/.zshrc
+    EOS
   end
 end
